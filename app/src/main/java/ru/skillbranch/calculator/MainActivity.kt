ab342d2b7c861135e025f801ac3cbc556b22a984
Egor.Ruban <Egor-Ruban@mail.ru>
2019-12-09 11:50:29 +0700

    apply kotlin style

--- app/src/main/java/ru/skillbranch/calculator/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/calculator/MainActivity.kt
@@ -4,131 +4,94 @@ import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var btnOne:Button
-    private lateinit var btnTwo:Button
-    private lateinit var btnThree:Button
-    private lateinit var btnFour:Button
-    private lateinit var btnFive:Button
-    private lateinit var btnSix:Button
-    private lateinit var btnSeven:Button
-    private lateinit var btnEight:Button
-    private lateinit var btnNine:Button
-    private lateinit var btnZero:Button
-
-    private lateinit var btnDivide:Button
-    private lateinit var btnMul:Button
-    private lateinit var btnSub:Button
-    private lateinit var btnSum:Button
-
-    private lateinit var btnDelete:Button
-
-    private lateinit var tvEquation:TextView
-    private lateinit var tvAnswer:TextView
-
-    private lateinit var btnBracketFirst:Button
-    private lateinit var btnBracketSecond:Button
-    private lateinit var btnDeleteChar:ImageButton
-    private lateinit var btnResult:Button
-    private lateinit var btnDot:Button
-
-    private lateinit var inputEquation:MutableList<String>
-
+    private lateinit var inputEquation: MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
         inputEquation = mutableListOf("")
-        tvEquation.movementMethod = ScrollingMovementMethod()
+        tv_equation.movementMethod = ScrollingMovementMethod()
     }
 
-
-
     override fun onClick(p0: View?) {
-        if(p0?.id==R.id.btn_operation_delete) {
-            tvEquation.text = ""
+        if (p0?.id == R.id.btn_operation_delete) {
+            tv_equation.text = ""
             inputEquation = mutableListOf("")
-            tvEquation.hint=""
-        }else if(p0?.id==btnDeleteChar.id){
-            if(inputEquation.last()!="") {
-                tvEquation.text = tvEquation.text.dropLast(1)
+            tv_equation.hint = ""
+        } else if (p0?.id == btn_delete_char.id) {
+            if (inputEquation.last() != "") {
+                tv_equation.text = tv_equation.text.dropLast(1)
                 val str = inputEquation[inputEquation.lastIndex]
                 inputEquation[inputEquation.lastIndex] = str.dropLast(1)
                 if (inputEquation[inputEquation.lastIndex] == "") {
                     inputEquation.removeAt(inputEquation.lastIndex)
                 }
             }
-        }else if(p0?.id==R.id.btn_result) {
-            if(inputEquation.last()!="") {
-                tvAnswer.text = "= ${translateToRPN()}"
-                tvEquation.hint = tvEquation.text
-                tvEquation.text = ""
+        } else if (p0?.id == R.id.btn_result) {
+            if (inputEquation.last() != "") {
+                tv_answer.text = "= ${translateToRPN()}"
+                tv_equation.hint = tv_equation.text
+                tv_equation.text = ""
                 inputEquation = mutableListOf("")
             }
-        }else{
+        } else {
             addToInput((p0 as Button).text.toString())
         }
     }
 
-    private fun addToInput(char:String){
-        tvEquation.append(char)
-        when(char){
-            "0","1","2","3","4","5","6","7","8","9","." ->
-                                                if(inputEquation.last().matches(Regex("([1-9]|[.])+"))){
-                                                    inputEquation[inputEquation.lastIndex] = inputEquation.last()+char
-                                                } else  inputEquation.add(char)
-            btnSub.text.toString() ->
-                                                if(inputEquation.last().matches(Regex("([1-9]|[.])+"))){
-                                                    inputEquation.add("-")
-                                                } else  inputEquation.add("~")
-            btnSum.text.toString()           -> inputEquation.add("+")
-            btnDivide.text.toString()        -> inputEquation.add("/")
-            btnMul.text.toString()           -> inputEquation.add("@")
-            btnBracketFirst.text.toString()  -> inputEquation.add("(")
-            btnBracketSecond.text.toString() -> inputEquation.add(")")
+    private fun addToInput(char: String) {
+        tv_equation.append(char)
+        when (char) {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." ->
+                if (inputEquation.last().matches(Regex("([1-9]|[.])+"))) {
+                    inputEquation[inputEquation.lastIndex] = inputEquation.last() + char
+                } else inputEquation.add(char)
+            btn_operation_sub.text.toString() ->
+                if (inputEquation.last().matches(Regex("([1-9]|[.])+"))) {
+                    inputEquation.add("-")
+                } else inputEquation.add("~")
+            btn_operation_sum.text.toString() -> inputEquation.add("+")
+            btn_operation_divide.text.toString() -> inputEquation.add("/")
+            btn_operation_mul.text.toString() -> inputEquation.add("@")
+            btn_bracket_1.text.toString() -> inputEquation.add("(")
+            btn_bracket_2.text.toString() -> inputEquation.add(")")
         }
 
     }
 
-    private fun checkInput():Boolean{
+    private fun checkInput(): Boolean {
         return false
     }
 
-    private fun translateToRPN():Double{
+    private fun translateToRPN(): Double {
         val operationStack = mutableListOf<String>()
         val equationInRPN = mutableListOf<String>()
-        for(word in inputEquation){
-            if(word==""){
-            }
-            else if(word.matches(Regex("([1-9]|[.])+"))){
+        for (word in inputEquation) {
+            if (word == "") {
+            } else if (word.matches(Regex("([1-9]|[.])+"))) {
                 equationInRPN.add(word)
-            }
-            else if(word=="~"){
+            } else if (word == "~") {
                 operationStack.add(word)
-            }
-            else if(word=="("){
+            } else if (word == "(") {
                 operationStack.add(word)
-            }
-            else if(word==")"){
+            } else if (word == ")") {
                 var operation = operationStack.last()
-                while(operation!="("){
+                while (operation != "(") {
                     equationInRPN.add(operation)
                     operationStack.removeAt(operationStack.lastIndex)
                     operation = operationStack.last()
                 }
                 operationStack.removeAt(operationStack.lastIndex)
-            }
-            else{
+            } else {
                 var operation = operationStack.lastOrNull() ?: " "
-                while(operation[0]>word[0] && operation!=" "){
+                while (operation[0] > word[0] && operation != " ") {
                     equationInRPN.add(operation)
                     operationStack.removeAt(operationStack.lastIndex)
                     operation = operationStack.lastOrNull() ?: " "
@@ -137,15 +100,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
         operationStack.reverse()
-        for(operation in operationStack){
+        for (operation in operationStack) {
             equationInRPN.add(operation)
         }
 
         return calculate(equationInRPN)
-
     }
 
-    private fun calculate(input:MutableList<String>):Double{
+    private fun calculate(input: MutableList<String>): Double {
         val workingStack = mutableListOf<Double>()
         for (word in input) {
             if (word.matches(Regex("([1-9]|[.])+"))) {
@@ -179,56 +141,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    private fun init(){
-        btnOne = btn_number_one
-        btnTwo = btn_number_two
-        btnThree = btn_number_three
-        btnFour = btn_number_four
-        btnFive = btn_number_five
-        btnSix = btn_number_six
-        btnSeven = btn_number_seven
-        btnEight = btn_number_eight
-        btnNine = btn_number_nine
-        btnZero = btn_number_zero
-        btnDivide = btn_operation_divide
-        btnMul = btn_operation_mul
-        btnSub = btn_operation_sub
-        btnSum = btn_operation_sum
-        btnDelete = btn_operation_delete
-
-        tvEquation = tv_equation
-        tvAnswer = tv_answer
-
-        btnBracketFirst = btn_bracket_1
-        btnBracketSecond = btn_bracket_2
-        btnDeleteChar = btn_delete_char
-        btnResult = btn_result
-        btnDot = btn_number_dot
-
-
-        btnOne.setOnClickListener(this)
-        btnTwo.setOnClickListener(this)
-        btnThree.setOnClickListener(this)
-        btnFour.setOnClickListener(this)
-        btnFive.setOnClickListener(this)
-        btnSix.setOnClickListener(this)
-        btnSeven.setOnClickListener(this)
-        btnEight.setOnClickListener(this)
-        btnNine.setOnClickListener(this)
-        btnZero.setOnClickListener(this)
-        btnDivide.setOnClickListener(this)
-        btnMul.setOnClickListener(this)
-        btnSub.setOnClickListener(this)
-        btnSum.setOnClickListener(this)
-        btnDelete.setOnClickListener(this)
-        btnResult.setOnClickListener(this)
-        btnDot.setOnClickListener(this)
-        btnBracketFirst.setOnClickListener(this)
-        btnBracketSecond.setOnClickListener(this)
-        btnDeleteChar.setOnClickListener(this)
+    private fun init() {
+        btn_number_one.setOnClickListener(this)
+        btn_number_two.setOnClickListener(this)
+        btn_number_three.setOnClickListener(this)
+        btn_number_four.setOnClickListener(this)
+        btn_number_five.setOnClickListener(this)
+        btn_number_six.setOnClickListener(this)
+        btn_number_seven.setOnClickListener(this)
+        btn_number_eight.setOnClickListener(this)
+        btn_number_nine.setOnClickListener(this)
+        btn_number_zero.setOnClickListener(this)
+        btn_operation_divide.setOnClickListener(this)
+        btn_operation_mul.setOnClickListener(this)
+        btn_operation_sub.setOnClickListener(this)
+        btn_operation_sum.setOnClickListener(this)
+        btn_operation_delete.setOnClickListener(this)
+        btn_result.setOnClickListener(this)
+        btn_number_dot.setOnClickListener(this)
+        btn_bracket_1.setOnClickListener(this)
+        btn_bracket_2.setOnClickListener(this)
+        btn_delete_char.setOnClickListener(this)
     }
 
-    fun onEggClick(v:View){
-        Toast.makeText(this,"there is nothing interesting",Toast.LENGTH_SHORT).show()
+    fun onEggClick(v: View) {
+        Toast.makeText(this, "there is nothing interesting", Toast.LENGTH_SHORT).show()
     }
 }
